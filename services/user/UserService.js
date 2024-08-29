9ab43d0ef5e635fb788a4d630eabbca670e8940e
navin1812ku <navin1812ku@gmail.com>
2024-08-29 20:32:06 +0530

    Updated UserService

--- services/user/UserService.js
+++ services/user/UserService.js
@@ -31,6 +31,7 @@ const UserService = {
     getUserDetails: async (userId) => {
         try {
             const userDetails = await User.findById(userId);
+            console.log(userId);
             return {
                 success: true,
                 message: `Fetched user details`,
